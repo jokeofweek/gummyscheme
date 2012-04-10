19ae92163fb8c737bd1f726c41dff6eaafd321b8
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2012-04-09 23:55:00 -0400

    Added car/cdr permutation families to the core file.

--- core/core.scm
+++ core/core.scm
@@ -1,2 +1,41 @@
+
 (define (not obj)
   (if obj #f #t))
+  
+(define (compose f g)
+  (lambda (arg)
+    (f (g arg))))
+  
+(define caar (compose car car))
+(define cadr (compose car cdr)) 
+
+(define cdar (compose cdr car))
+(define cddr (compose cdr cdr)) 
+
+(define caaar (compose car caar))
+(define caadr (compose car cadr))
+(define cadar (compose car cdar))
+(define caddr (compose car cddr)) 
+
+(define cdaar (compose cdr caar))
+(define cdadr (compose cdr cadr))
+(define cddar (compose cdr cdar))
+(define cdddr (compose cdr cddr)) 
+
+(define caaaar (compose caar caar))
+(define caaadr (compose caar cadr))
+(define caadar (compose caar cdar))
+(define caaddr (compose caar cddr))
+(define cadaar (compose cadr caar))
+(define cadadr (compose cadr cadr))
+(define caddar (compose cadr cdar))
+(define cadddr (compose cadr cddr)) 
+
+(define cdaaar (compose cdar caar))
+(define cdaadr (compose cdar cadr))
+(define cdadar (compose cdar cdar))
+(define cdaddr (compose cdar cddr))
+(define cddaar (compose cddr caar))
+(define cddadr (compose cddr cadr))
+(define cdddar (compose cddr cdar))
+(define cddddr (compose cddr cddr))
